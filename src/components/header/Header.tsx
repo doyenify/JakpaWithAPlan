995d365f947f5a6911f730fbe18780f23e0f469d
TecFesco <108395865+TecFesco@users.noreply.github.com>
2023-03-31 13:33:27 +0100

    What We Offer Page
    What we offer section
    Each service section
    about page linked
    read more linked
    powered by doyenify included

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Display } from 'react-bootstrap-icons';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
@@ -9,7 +10,8 @@ const Header = () => {
     <div>
      <Navbar  expand="lg" style={{backgroundColor: "#8CACD2"}}>
       <Container fluid>
-        <Navbar.Brand className='ms-5' href="#" style={{color:"#ffffff", fontSize:"20px" }}>JakpaWithAPlan</Navbar.Brand>
+        <Navbar.Brand className='ms-5' href="#" style={{color:"#ffffff", fontSize:"20px", fontWeight: "bold" }}>JakpaWithAPlan <br /> 
+        <h5 style={{color:"#044163", fontSize:"12px"}}>Powered by Doyenify</h5> </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
