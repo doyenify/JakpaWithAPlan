75a8f00c939852d10e7fad8bc25ff479d665e6ad
Ifedolapo Ayoola <dolybim2gud@gmail.com>
2023-04-03 22:51:34 +0100

    Testimonial Section

--- src/components/testimonial/Testimonial.tsx
+++ src/components/testimonial/Testimonial.tsx
@@ -1,8 +1,62 @@
-import React from 'react'
+import {Carousel} from 'react-bootstrap';
+import "./testimonial.css";
+
+const testimonyArray = [
+`“Cum nisl tellus risus bibendum vel. . . Pellentesque suspendisse
+  nunc proin ultrices neque sagittis.”`,
+`“Cum nisl tellus risus bibendum vel. . . Pellentesque suspendisse
+  nunc proin ultrices neque sagittis.”`,
+`“Nisl tellus risus bibendum vel. . . Pellentesque suspendisse
+  nunc proin ultrices neque sagittis.”`,
+`“Cum nisl tellus risus bibendum vel. . . Pellentesque suspendisse
+  nunc proin ultrices neque sagittis.”`
+]
 
 const Testimonial = () => {
   return (
-    <div>Testimonial</div>
+    <div className="bg-black d-flex justify-content-center align-items-center" id='testimonial-wrapper' style={{ height: "60vh" }}>
+      <div className="row testimonial-section text-white">
+      <Carousel>
+      <Carousel.Item>
+        <div className='carousel-div'>
+        <Carousel.Caption className='caption'>
+          <h3>Testimonial</h3>
+          <p>{testimonyArray[0]}</p>
+          <p>Ifedolapo Ayoola</p>
+        </Carousel.Caption>
+        </div>
+      </Carousel.Item>
+      <Carousel.Item>
+      <div className='carousel-div'>
+        <Carousel.Caption className='caption'>
+          <h3>Testimonial</h3>
+          <p>{testimonyArray[1]}</p>
+          <p>Timilehin Ogunwole</p>
+        </Carousel.Caption>
+        </div>
+      </Carousel.Item>
+      <Carousel.Item>
+        <div className='carousel-div'>
+        <Carousel.Caption className='caption'>
+          <h3>Testimonial</h3>
+          <p>{testimonyArray[2]}</p>
+          <p>IfeOluwa Olagbemi</p>
+        </Carousel.Caption>
+        </div>
+      </Carousel.Item>
+      <Carousel.Item>
+        <div className='carousel-div'>
+        <Carousel.Caption className='caption'>
+          <h3>Testimonial</h3>
+          <p>{testimonyArray[3]}</p>
+          <p>Solagbade Enitilo</p>
+        </Carousel.Caption>
+        </div>
+      </Carousel.Item>
+    </Carousel>
+        
+      </div>
+    </div>
   )
 }
 
