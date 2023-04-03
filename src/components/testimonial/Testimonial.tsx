c097cd36e0c3e14bea3268bbdafb33544936019d
Ifedolapo Ayoola <dolybim2gud@gmail.com>
2023-04-03 22:58:25 +0100

    code fix

--- src/components/testimonial/Testimonial.tsx
+++ src/components/testimonial/Testimonial.tsx
@@ -17,42 +17,42 @@ const Testimonial = () => {
     <div className="bg-black d-flex justify-content-center align-items-center" id='testimonial-wrapper' style={{ height: "60vh" }}>
       <div className="row testimonial-section text-white">
       <Carousel>
-      <Carousel.Item>
-        <div className='carousel-div'>
-        <Carousel.Caption className='caption'>
-          <h3>Testimonial</h3>
-          <p>{testimonyArray[0]}</p>
-          <p>Ifedolapo Ayoola</p>
-        </Carousel.Caption>
-        </div>
-      </Carousel.Item>
-      <Carousel.Item>
-      <div className='carousel-div'>
-        <Carousel.Caption className='caption'>
-          <h3>Testimonial</h3>
-          <p>{testimonyArray[1]}</p>
-          <p>Timilehin Ogunwole</p>
-        </Carousel.Caption>
-        </div>
-      </Carousel.Item>
-      <Carousel.Item>
-        <div className='carousel-div'>
-        <Carousel.Caption className='caption'>
-          <h3>Testimonial</h3>
-          <p>{testimonyArray[2]}</p>
-          <p>IfeOluwa Olagbemi</p>
-        </Carousel.Caption>
-        </div>
-      </Carousel.Item>
-      <Carousel.Item>
-        <div className='carousel-div'>
-        <Carousel.Caption className='caption'>
-          <h3>Testimonial</h3>
-          <p>{testimonyArray[3]}</p>
-          <p>Solagbade Enitilo</p>
-        </Carousel.Caption>
-        </div>
-      </Carousel.Item>
+        <Carousel.Item>
+          <div className='carousel-div'>
+          <Carousel.Caption className='caption'>
+            <h3>Testimonial</h3>
+            <p>{testimonyArray[0]}</p>
+            <p>Ifedolapo Ayoola</p>
+          </Carousel.Caption>
+          </div>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div className='carousel-div'>
+          <Carousel.Caption className='caption'>
+            <h3>Testimonial</h3>
+            <p>{testimonyArray[1]}</p>
+            <p>Timilehin Ogunwole</p>
+          </Carousel.Caption>
+          </div>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div className='carousel-div'>
+          <Carousel.Caption className='caption'>
+            <h3>Testimonial</h3>
+            <p>{testimonyArray[2]}</p>
+            <p>IfeOluwa Olagbemi</p>
+          </Carousel.Caption>
+          </div>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div className='carousel-div'>
+          <Carousel.Caption className='caption'>
+            <h3>Testimonial</h3>
+            <p>{testimonyArray[3]}</p>
+            <p>Solagbade Enitilo</p>
+          </Carousel.Caption>
+          </div>
+        </Carousel.Item>
     </Carousel>
         
       </div>
