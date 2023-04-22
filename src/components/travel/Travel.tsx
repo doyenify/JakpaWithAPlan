d0ff56495df594f622173b6a3eca275bf7f5a088
Ifedolapo Ayoola <dolybim2gud@gmail.com>
2023-04-22 14:07:15 +0100

    effects fix

--- src/components/travel/Travel.tsx
+++ src/components/travel/Travel.tsx
@@ -47,7 +47,7 @@ const handleClick = (index: number) => {
                     <img src={travel_img_5} alt="travel img"></img>
                     <h5 style={{ fontSize: "1.25rem", position: "absolute", bottom: "16px", right: "5%", background: "rgba(0,0,0,0.3)", padding: "5px", borderRadius: "5px"}}>Career</h5>
                 </Col>
-                <Col className='col-sm-4' style={{ position: "relative" }}>
+                <Col className='col-sm-4 travel-img-parent' onClick={() => handleClick(5)} style={{ position: "relative" }}>
                     <img src={travel_img_6} alt="travel img"></img>
                     <h5 style={{ fontSize: "1.25rem", position: "absolute", bottom: "16px", right: "5%", background: "rgba(0,0,0,0.3)", padding: "5px", borderRadius: "5px" }}>Dream Job</h5>
                 </Col>
@@ -58,37 +58,37 @@ const handleClick = (index: number) => {
             <div className='travel-content-wrapper'>
               <h2>Relocate With A Plan </h2>
               <ul className='travel-list'>
-                <li ref={skillRef} className={highlight === 0 ? 'highlight' : 'un-highlight'}>Build a skill 
+                <li ref={skillRef} className={highlight === 2 ? 'highlight' : 'un-highlight'}>Build a skill 
                   <span className='travel-skill'>Building a skill is like building a muscle - 
                       the more you work on it, the stronger it becomes.
                   </span>
                 </li>
-                <li className={highlight === 1 ? 'highlight' : 'un-highlight'}>Build Competence 
+                <li className={highlight === 0 ? 'highlight' : 'un-highlight'}>Build Competence 
                   <span className='travel-skill'>
                     Building competence is a journey, not a destination. 
                     It requires dedication, persistence, and a commitment to continuous learning.
                   </span>
                 </li>
-                <li className={highlight === 2 ? 'highlight' : 'un-highlight'}>Increase Income
+                <li className={highlight === 3 ? 'highlight' : 'un-highlight'}>Increase Income
                   <span className='travel-skill'>
                     We assist individuals by ensuring improvement in 
                     ability to explore various ways of increasing their income.
                   </span>
                 </li>
-                <li className={highlight === 3 ? 'highlight' : 'un-highlight'}>Build A Career 
+                <li className={highlight === 4 ? 'highlight' : 'un-highlight'}>Build A Career 
                   <span className='travel-skill'>
                     Building a successful career requires self-awareness, skill development, 
                     dedication, hard work, continuous learning, adaptability, and a clear vision 
                     of one's goals.
                   </span>
                 </li>
-                <li className={highlight === 4 ? 'highlight' : 'un-highlight'}>Dream Job 
+                <li className={highlight === 5 ? 'highlight' : 'un-highlight'}>Dream Job 
                   <span className='travel-skill'>
                     Your dream job is not just a career, it's a reflection of your passion, 
                     purpose, and values that brings fulfillment and satisfaction to your life.
                   </span>
                 </li>
-                <li>Acquire Experience 
+                <li className={highlight === 1 ? 'highlight' : 'un-highlight'}>Acquire Experience 
                   <span className='travel-skill'>
                     Experience is the priceless currency of wisdom, 
                     earned through the trials and triumphs of life's journey,
