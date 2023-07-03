51ea8d72a6324a4daadfdb33db7c87cf2897c9a8
TayeCaleb <calebmide@gmail.com>
2023-07-03 16:37:31 +0100

    added slider

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,24 +1,66 @@
-import {Button, Card} from 'react-bootstrap';
+import { Card, Button } from "react-bootstrap";
+import {Carousel} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./hero.css";
 
 const Hero = () => {
   return (
     <div>
-      <div className='hero-card-container' style={{backgroundColor: "#121212"}}>
-      <Card className="mt-1  bg-dark text-white" style={{ borderRadius: "0px", height: "100vh" }}>
-      <Card.Img src="./img/hero.png" alt="Card image" style={{ height: "100%", objectFit: "cover"}}/>
-      <Card.ImgOverlay className='px-5 d-flex flex-column align-items justify-content-center'>
-        <Card.Title className="hero-title" style={{ fontSize:"2.5rem" }}>Moving To Greener Pastures Is Not <br /> 
-         An Escape From Your Current Situation, <br />  But Rather A Step Towards Your Desired <br /> Destination
-        </Card.Title>
-        <div className='mt-3 hero-btn'>
-            <Button  className='hero-about-btn  px-5 ' href='#aboutuspage' style={{backgroundColor: "#044163"}}>About Us</Button>
-            <Button  className='hero-offer-btn mx-2 px-5' href='#whatweoffer' style={{backgroundColor: "transparent"}}>What We Offer</Button>
-          </div>
-      </Card.ImgOverlay>
-    </Card>
-    </div>
+      <Carousel >
+              <Carousel.Item >
+              <div className = "cardcon"  >
+                <img src="./img/hero.png" alt="...">
+
+                </img>
+                  <Carousel.Caption className='caption carousel-div'>
+                    <br/>
+                    <br/>
+                    <div className='hero-card-container '>
+                    <Card className="bg-transparent  bg-dark text-white " style={{padding: "0", borderColor: "#ffffff00"}}>
+                    <Card.Title className="hero-title text-width">Moving To Greener Pastures Is Not <br /> 
+                      An Escape From Your Current Situation, <br />  But Rather A Step Towards Your Desired <br /> Destination
+                      </Card.Title>
+                      <div className='mt-3 hero-btn'>
+                          <Button  className='hero-about-btn  px-5 ' href='#aboutuspage' style={{backgroundColor: "#044163"}}>About Us</Button>
+                          <Button  className='hero-offer-btn mx-2 px-5' href='#whatweoffer' style={{backgroundColor: "transparent"}}>What We Offer</Button>
+                        </div>
+                    
+                  </Card>
+                  </div>
+                    
+                  </Carousel.Caption>
+                </div>
+              </Carousel.Item>
+
+              <Carousel.Item >
+              <div className = "cardcon"  >
+                <img src="./img/doyenacademy.png" className="cimg" alt="...">
+
+                </img>
+                  <Carousel.Caption className='caption carousel-div'>
+                    <br/>
+                    <br/>
+                    <div className='hero-card-container '>
+                    <Card className="bg-transparent  bg-dark text-white " style={{padding: "0", borderColor: "#ffffff00"}}>
+                    <div className="doyenifyacd-text" >Doyenify Academy</div>
+                    <Card.Title className="hero-title text-width2">At this Academy we offer you courses that will help you thrieve in the job market,
+                    <br /> You don't have to be great to start, <br />but you have to start to be great - zig Ziglar.
+                    </Card.Title>
+                      <div className='mt-3 hero-btn'>
+                          <Button  className='doyenifyacd-about-btn  px-5 ' href='#aboutuspage' >Doyenify Academy</Button>
+                          <Button  className='doyenifyacd-offer-btn mx-2 px-5' href='#whatweoffer' >What We Offer</Button>
+                        </div>
+                    
+                  </Card>
+                  </div>
+                    
+                  </Carousel.Caption>
+                </div>
+              </Carousel.Item>
+              
+              
+          </Carousel>
+     
     </div>
   )
 }
